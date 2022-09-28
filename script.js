c4335dc52399ef8e6440bc036f1bc90c6b7c9ea6
RainbowToad <chad_spilker@hotmail.com>
2022-09-28 15:37:16 -0700

    Created arrays

--- script.js
+++ script.js
@@ -12,7 +12,16 @@ function generatePassword () {
     return;
   }
 
-  window.confirm("Would you like to include lowercase letters?")
+  var passLower = window.confirm("Would you like to include lowercase letters?")
+  var passUpper = window.confirm("Would you like to include Uppercase letters?")
+  var passNumber = window.confirm("Would you like to include numbers?")
+  var passSpecial = window.confirm("Would you like to include special characters?")
+
+  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var symbols = ["@", "*", "#", "!", "&", "%", "$"]
+  var lowers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var uppers = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
 }
 
 // Write password to the #password input
