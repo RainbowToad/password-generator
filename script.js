313abb9610c750464d70bcaa9d4f033445814fea
RainbowToad <chad_spilker@hotmail.com>
2022-09-28 13:59:45 -0700

    Passlength check created

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+function generatePassword () {
+  
+  var passLength = window.prompt("Character Length?")
+  
+  if (passLength<8 || passLength>128) {
+    window.alert("Must be between 8-128 characters")
+    return;
+  }
+
+  window.confirm("Would you like to include lowercase letters?")
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,7 +26,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-function generatePassword() {
-  return 'Chad is Cool';
-}
